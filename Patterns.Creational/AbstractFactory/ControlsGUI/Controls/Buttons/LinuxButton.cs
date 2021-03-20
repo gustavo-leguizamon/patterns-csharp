b15c802b826483d024ba200c00ee1d0cdc7e08ae
Gustavo <Hukummi@live.com>
2021-03-19 21:18:36 -0300

    Abstract Factory - Add controlsGUI example

---
+++ Patterns.Creational/AbstractFactory/ControlsGUI/Controls/Buttons/LinuxButton.cs
@@ -0,0 +1,15 @@
+﻿using Patterns.Creational.AbstractFactory.ControlsGUI.Interfaces.Controls.Buttons;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.Creational.AbstractFactory.ControlsGUI.Controls.Buttons
+{
+    public class LinuxButton : IButton
+    {
+        public void Paint()
+        {
+            Console.WriteLine("You have created LinuxButton.");
+        }
+    }
+}
